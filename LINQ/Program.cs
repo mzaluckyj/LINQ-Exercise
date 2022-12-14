e12b7b3878fa2ff97bf7e156c1a9a5cdf93c45ea
Michael <m.zaluckyj@gmail.com>
2022-12-14 13:53:11 -0500

    exercise 2

--- LINQ/Program.cs
+++ LINQ/Program.cs
@@ -4,7 +4,18 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            List<string> videoGames = new List<string>()
+            { "Halo", "Call of Duty", "Minecraft", "Legend of Zelda", "Super Smash Bros."};
+
+            var gamesList = videoGames.OrderByDescending(x => x.Length).ToList();
+                         
+
+            foreach (var game in gamesList) 
+            {
+            Console.WriteLine(game);
+            }
+            Console.WriteLine();
+           // IEnumerable<string> videoGames2 = new List<string>();
         }
     }
 }
